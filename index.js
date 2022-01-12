16360688b2b23cc0628716f2f13975db00a53b10
taauntik <devauntik@gmail.com>
2022-01-12 21:53:52 +0600

    changed some code in socket again

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = require("express")();
 const server = require("http").createServer(app);
 const cors = require("cors");
 
-app.use(cors());
 const io = require("socket.io")(server, {
   cors: {
     origin: "*",
@@ -10,24 +9,27 @@ const io = require("socket.io")(server, {
   },
 });
 
+app.use(cors());
+
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send("Server is running now");
+  res.send("Running");
 });
 
 io.on("connection", (socket) => {
   socket.emit("me", socket.id);
+
   socket.on("disconnect", () => {
-    socket.broadcast.emit("call ended");
+    socket.broadcast.emit("callEnded");
   });
 
-  socket.on("calluser", ({ userToCall, signalData, from, name }) => {
-    io.to(userToCall).emit("calluser", { signal: signalData, from, name });
+  socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+    io.to(userToCall).emit("callUser", { signal: signalData, from, name });
   });
 
-  socket.on("answercall", (data) => {
-    io.to(data.to).emit("callaccepted", data.signal);
+  socket.on("answerCall", (data) => {
+    io.to(data.to).emit("callAccepted", data.signal);
   });
 });
 
