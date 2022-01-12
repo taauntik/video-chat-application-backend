44eb59a13a7f90b123ac0d0ad5c6f0c7938d00cb
taauntik <devauntik@gmail.com>
2022-01-12 21:37:12 +0600

    changed some code

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: "https://video-chat-project.netlify.app/",
     methods: ["GET", "POST"],
   },
 });
